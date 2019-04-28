6a442ab28cb416f4e3c4c8d69c0bcabf5be2c397
Vinícius Albuquerque <mvca2001@gmail.com>
2019-04-28 11:29:51 -0300

    adding usuarios module

---
+++ src/app/gerencia/usuarios/usuarios.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsuariosRoutingModule } from './usuarios-routing.module';
+import { UsuarioListComponent } from './usuario-list/usuario-list.component';
+import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
+
+@NgModule({
+  declarations: [UsuarioListComponent, UsuarioFormComponent],
+  imports: [
+    CommonModule,
+    UsuariosRoutingModule
+  ]
+})
+export class UsuariosModule { }
